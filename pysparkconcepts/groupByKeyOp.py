04630fc2660cdde3bd2d93b9c33388cda7c90451
saksham123saxena <98534639+saksham123saxena@users.noreply.github.com>
2023-10-13 18:07:25 +0530

    added groupbykey

---
+++ pysparkconcepts/groupByKeyOp.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession
+
+spark=SparkSession.builder.master("local").appName("GroupByKey").getOrCreate()
+
+df=spark.sparkContext.textFile("/Users/sakshamsaxena/PycharmProjects/TutPyspark/sourcedata/orderitem.txt")
+for x in df.collect():
+    print(x)
+
+dateGroup=df.map(lambda x : (x.split(',')[1],int(x.split(',')[2]))).groupByKey()
+
+
+for i in dateGroup.mapValues(sum).collect():
+    print(i)
+
+dateGroup1=dateGroup.map(lambda x : (x[0],sum(x[1])))
+
+for x in dateGroup1.collect():
+    print(x)
+
