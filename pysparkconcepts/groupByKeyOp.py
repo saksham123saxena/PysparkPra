ab282c4788eb438fc8f1276ec547d828f61e6cb8
saksham123saxena <98534639+saksham123saxena@users.noreply.github.com>
2023-10-25 12:47:58 +0530

    added some other concept

--- pysparkconcepts/groupByKeyOp.py
+++ pysparkconcepts/groupByKeyOp.py
@@ -12,8 +12,14 @@ dateGroup=df.map(lambda x : (x.split(',')[1],int(x.split(',')[2]))).groupByKey()
 for i in dateGroup.mapValues(sum).collect():
     print(i)
 
+
 dateGroup1=dateGroup.map(lambda x : (x[0],sum(x[1])))
 
 for x in dateGroup1.collect():
     print(x)
+
+
+'''
+skdms
+'''
 
